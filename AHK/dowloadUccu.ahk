2c06653e54e9890b7f1dcddd6125918792d3d4f2
James Fluckiger <jfluckiger@cloudbees.com>
2023-08-27 09:48:32 -0600

    Some Selenium

---
+++ AHK/dowloadUccu.ahk
@@ -0,0 +1,5 @@
+#SingleInstance force
+CoordMode, Mouse, Screen
+
+
+RunWait, "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
--- Selenium/seleniumUccu.py
+++ Selenium/seleniumUccu.py
@@ -1,26 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def get_account_data():
+# def get_account_data():
 
-    driver = webdriver.Chrome()
-
-    driver.get("https://www.uccu.com")
-    driver.implicitly_wait(5)
-
-    userid_box = driver.find_element(by=By.NAME, value="user_id")
-    password_box = driver.find_element(by=By.NAME, value="password")
-    sign_in_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-    userid_box.send_keys("1095729")
-    password_box.send_keys("M.,mlkjpou75")
-    sign_in_button.click()
-    driver.implicitly_wait(5)
-
-
-    title = driver.title
-    assert title == "Utah Community Federal Credit Union: Homeasdf"
+driver = webdriver.Chrome()
+driver.get("https://www.uccu.com")
+driver.implicitly_wait(5)
+userid_box = driver.find_element(by=By.ID, value="QuickLoginHeader_user_id")
+password_box = driver.find_element(by=By.ID, value="QuickLoginHeader_password")
+sign_in_form = driver.find_element(by=By.ID, value="Q2OnlineLogin")
+# sign_in_button = driver.find_element_by_css_selector(".Button")
+userid_box.send_keys("1095729")
+password_box.send_keys("M.,mlkjpou75")
+sign_in_form.submit()
+driver.implicitly_wait(5)
+title = driver.title
+assert title == "Utah Community Federal Credit Union: Homeasdf"
 
 
 
-    driver.quit()
+    # driver.quit()
